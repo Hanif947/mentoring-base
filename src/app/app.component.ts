c780a4f502c7b218739ec15f82a89e706e5361ac
Ismail <usellpro1@gmail.com>
2025-06-11 11:48:13 +0100

    Feat after 6th lesson: 1. Added a function, linked it all to a template  so that the text "About the company" is taken from  the aboutCompany class field; 2. Added a button that will hide/show the main  large picture banner; 3. I made the numbers 1 2 3 4 5 to be replaced  by 5 4 3 2 1 using a loop through the array  via *ngFor; 4. Added a "Change text" button, when clicked,  the menu text will become small/large.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+ 
+const newPages = [ 5, 4, 3, 2, 1 ];
+
+const upperCaseMenuItems = [ 'Каталог', 'Стройматериалы ', 'Инструменты ', 'Электрика', 'Интерьер и одежда' ];
+
+function menuItem (item: string) {
+  return item
+}
+
+const headermMenuItem: string = menuItem('О компании')
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ NgIf, NgFor ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+
+  isShowCatalog = !false;
+  isShowBanner = !false;
+  isUpperCase = true;
+
+  menuItems = upperCaseMenuItems;
+
+  changeMenuText() {
+    this.menuItems = upperCaseMenuItems.map(
+      item => this.isUpperCase ? item.toLowerCase() : item.toUpperCase()
+    )
+    this.isUpperCase = !this.isUpperCase;
+  }
+
+  readonly aboutCompany = headermMenuItem;
+
+  readonly headerItem1 = 'Главная';
+  readonly headerItem3 = 'Каталог';
+
+  readonly newPages = newPages;
+
 }
